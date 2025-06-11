32cab4867daf05613acdb82b4725fe4a48fd59db
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-12 00:32:29 +0530

    Topic Variables & Datatypes

---
+++ 01 Variable/variable.py
@@ -0,0 +1,13 @@
+name = "Lakshya"
+age = 18
+height = 5.11
+
+print("My Name is :",name)
+print("My Age is :",age)
+print("My height is :",height)
+print()
+
+number1 = 100
+number2 = 200
+result = number1+number2
+print("Result is : - ",result)
